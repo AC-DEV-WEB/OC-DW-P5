0a11cf0af17e2448ebf0fa7855a300d68d52a991
AC-DEV-WEB <ac.dev.webapp@gmail.com>
2020-04-18 20:48:14 +0200

    Ajout du prix
    Ajout du prix sur la liste des produits et reprise de la mise en forme de la page d'accueil.

--- front-end/js/furnitures.js
+++ front-end/js/furnitures.js
@@ -21,9 +21,9 @@ request.onload = function () {
     name.classList.add("products-name");
 
     // prix des produits
-    const name = document.createElement("p");
-    name.textContent = furnitures[i].name;
-    name.classList.add("products-name");
+    const price = document.createElement("p");
+    price.textContent = "Prix : " + furnitures[i].price + " €";
+    price.classList.add("products-price");
 
     // images des produits
     const image = document.createElement("img");
@@ -38,8 +38,9 @@ request.onload = function () {
     link.href = "./product.html?id=" + furnitures[i]._id;
     button.appendChild(image);
     button.appendChild(name);
+    button.appendChild(price);
     button.appendChild(link);
-    button.classList.add("products-container");
+    button.classList.add("products-content");
     show.appendChild(button);
   }
 }
